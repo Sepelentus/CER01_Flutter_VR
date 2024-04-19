51daf6a2ae0888f6a75fac407a13393fe3308745
Sepelentus <vicente.ramirez.gonzalez@gmail.com>
2024-04-19 19:29:26 -0400

    Added image and welcome text to main page

--- cer01_alta_mesa/lib/pages/main_page.dart
+++ cer01_alta_mesa/lib/pages/main_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatelessWidget {
@@ -7,10 +9,52 @@ class MainPage extends StatelessWidget {
   MainPage({required this.usuario, required this.icon});
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 248, 248, 248),
       body: Container(
-        child: Column(
+        padding: EdgeInsets.symmetric(vertical: 150,horizontal: 125),
+        child: Row(
           children: <Widget>[
-            Align()
+            // Imagen de perfil
+            Flexible(
+              child: Container( 
+                child: Column(
+                  children: <Widget>[
+                  CircleAvatar(
+                  backgroundImage: AssetImage(icon),
+                  radius: 32
+                ),
+                  ],
+                ),
+                
+              ),
+            ),
+            // Texto de perfil
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(left: 15, top: 7.5),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          'Bienvenido',
+                          style: GoogleFonts.robotoSlab(),
+                          textScaler: TextScaler.linear(1.2),
+                          ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          usuario,
+                          style: GoogleFonts.robotoSlab(),),
+                      ],
+                    )
+                  ]
+                ) ,
+              ),
+            )
           ],
         ),
       ),
